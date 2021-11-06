d99e3e763de777431d53b442a44e6b7165dd3005
Dary Castillo <dcastillo.mp@gmail.com>
2021-11-06 10:21:47 -0600

    manejo de error de consulta

--- api/Consultas.js
+++ api/Consultas.js
@@ -3,11 +3,7 @@ const axios = require("axios");
 const BASE_URL = process.env.API;
 
 const getCursosByCarnet = async (carnet) => {
-  const {
-    data: {
-      Estudiantes: { cursos },
-    },
-  } = await axios({
+  const { data } = await axios({
     method: "GET",
     url: `${BASE_URL}/proyectfinal/api/Estudiante`,
     headers: {
@@ -19,9 +15,17 @@ const getCursosByCarnet = async (carnet) => {
     },
   });
 
-  return cursos
-    .map((curso, index) => `📚 ${index + 1}. ${curso.Curso_nombre}`)
-    .join("\n\n");
+  const cursos = data.Estudiantes?.cursos;
+  let result = "";
+  if (!cursos || !cursos.length) {
+    result = `No existen cursos para el carnet: ${carnet}`;
+  } else {
+    result = cursos
+      .map((curso, index) => `📚 ${index + 1}. ${curso.Curso_nombre}`)
+      .join("\n\n");
+  }
+
+  return result;
 };
 
 const getNotasByCarnet = async (carnet) => {
@@ -37,13 +41,20 @@ const getNotasByCarnet = async (carnet) => {
       carnet,
     },
   });
-  return Nota.reduce((acc, val, index) => {
-    const { Curso_nombre } = val.id_curso;
-    const { nota } = val;
 
-    acc.push(`📝 ${index + 1} ${Curso_nombre}: ${nota}`);
-    return acc;
-  }, []).join("\n\n");
+  let result = "";
+  if (!Object.keys(Nota).length) {
+    result = `No existen notas para el carnet: ${carnet}`;
+  } else {
+    result = Nota.reduce((acc, val, index) => {
+      const { Curso_nombre } = val.id_curso;
+      const { nota } = val;
+
+      acc.push(`📝 ${index + 1} ${Curso_nombre}: ${nota}`);
+      return acc;
+    }, []).join("\n\n");
+  }
+  return result;
 };
 
 module.exports = {
